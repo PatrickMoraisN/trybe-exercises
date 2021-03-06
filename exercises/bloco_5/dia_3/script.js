d8c68675057f2344e69f8cfb039f9383ef5a0633
Patrick Morais <ppternunes@gmail.com>
2021-03-06 12:13:14 -0300

    é tetra porraa

--- exercises/bloco_5/dia_3/script.js
+++ exercises/bloco_5/dia_3/script.js
@@ -114,3 +114,117 @@ function friday() {
 friday()
 
 
+function dayMouseOver() {
+  let day = document.querySelector('#days')
+
+  day.addEventListener('mouseover', function (event) {
+    event.target.style.fontSize = '30px'
+    event.target.style.fontWeight = '600'
+  })
+}
+dayMouseOver();
+
+function dayMouseOut() {
+
+  let day = document.querySelector('#days')
+
+  day.addEventListener('mouseout', function (event) {
+    event.target.style.fontSize = '20px'
+    event.target.style.fontWeight = '200'
+  })
+
+}
+dayMouseOut()
+
+
+function tasks(param) {
+  let divPai = document.querySelector('.my-tasks')
+
+  let tarefaCriada = document.createElement('span');
+  tarefaCriada.innerHTML = param;
+  divPai.appendChild(tarefaCriada)
+
+}
+tasks('cozinhar')
+
+function colorChange(color) {
+  let divPai = document.querySelector('.my-tasks')
+  let createDiv = document.createElement('div')
+  createDiv.className = 'task'
+  createDiv.style.backgroundColor = color
+
+  divPai.appendChild(createDiv)
+}
+colorChange('red')
+
+function taskSelected() {
+  let task = document.querySelector('.task')
+
+  task.addEventListener('click', function(event){
+    if (task.classList.contains('selected') === true) {
+      task.classList.remove('selected')
+    } else {
+      task.classList.add('selected')
+    }
+  })
+}
+taskSelected()
+
+
+function changeDayColor(){
+  let day = document.querySelector('#days')
+  let taskSelect = document.querySelector('.task')
+  let colorTask = 'red'
+  let colorDefault = 'rgb(119,119,119)'
+  
+  day.addEventListener('click', function(event){
+    if(event.target.style.color === colorTask && taskSelect.classList.contains('selected')){
+      event.target.style.color = colorDefault
+    } else if(taskSelect.classList.contains('selected')){
+      event.target.style.color = colorTask
+    }
+  })
+
+}
+changeDayColor()
+
+
+function addNewTask() {
+let getBtnAdd = document.getElementById('btn-add')
+let getTaskList = document.querySelector('.task-list')
+let getInputField = document.querySelector('#task-input')
+
+
+getBtnAdd.addEventListener('click', function(){
+if(getInputField.value.length > 0){
+  let newLi = document.createElement('li')
+  newLi.innerText = getInputField.value
+
+  getTaskList.appendChild(newLi)
+  getInputField.value = ''
+} else {
+  alert('Digite algo!!')
+}
+})
+
+getInputField.addEventListener('keyup', function(event){
+if(event.keyCode === 13 && getInputField.value.length > 0){
+  let newLi = document.createElement('li')
+  newLi.innerText = getInputField.value
+
+  getTaskList.appendChild(newLi)
+  getInputField.value = ''
+
+} else if (event.keyCode === 13 && getInputField.value.length === 0){
+  alert('Digite algo!')
+}
+
+})
+
+
+
+}
+addNewTask()
+
+
+
