4ef256aaee07ca0cfcbc4d3c4236895806cb9381
Patrick Morais <ppternunes@gmail.com>
2021-04-07 09:14:21 -0300

    ex5 reduce

--- exercises/bloco_8/dia_4/ex5.js
+++ exercises/bloco_8/dia_4/ex5.js
@@ -7,16 +7,20 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
-function containsA() {
-  let num = 0;
-for(let i = 0; i<names.length; i++) {
-  for(let b = 0; b<names[i].length; b++){
-    if(names[i][b] === 'A' || names[i][b] === 'a'){
-      num++;
-    }
-  }
-}
-return num
+//function containsA() {
+//  let num = 0;
+//for(let i = 0; i<names.length; i++) {
+ // for(let b = 0; b<names[i].length; b++){
+//    if(names[i][b] === 'A' || names[i][b] === 'a'){
+//      num++;
+ //   }
+//  }
+//}
+//return num
+//}
+
+function containsA(){
+  return names.reduce((acc, elem) => acc + elem.split('').reduce((acc2, elem2) => elem2 === 'A' || elem2 === 'a' ? acc2 + 1 : acc2, 0 ), 0)
 }
 
 assert.deepStrictEqual(containsA(), 20);
