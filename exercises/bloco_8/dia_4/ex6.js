00ace38f7e18722565f06a7dbecdfdfe6fa2ef77
Patrick Morais <ppternunes@gmail.com>
2021-04-07 09:36:39 -0300

    ex6

---
+++ exercises/bloco_8/dia_4/ex6.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+const arrayGrades = grades.map(elem => elem.reduce((acc, elem2) => (acc+elem2), 0)/elem.length)
+const obj = students.map((elem, index) => ({name: elem , average: arrayGrades[index]}))
+
+return obj;
+}
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
