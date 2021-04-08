6890ba631c080a4168b32eef037e787210fe9a74
Patrick Morais <ppternunes@gmail.com>
2021-04-08 13:26:24 -0300

    ex4

---
+++ exercises/bloco_8/dia_5/ex4.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+const filterPeople = (arr) =>  arr.filter(({bornIn, nationality}) => 
+  nationality === 'Australian' && bornIn < 2001
+  )
+
+
+const filteredPeople = filterPeople(people);
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
