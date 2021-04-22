7bd3d8bfcaa4f1a02996927d582f68113442e16a
Patrick Morais <ppternunes@gmail.com>
2021-04-21 21:21:46 -0300

    fecth async

--- exercises/bloco_9/dia_2/apiScript.js
+++ exercises/bloco_9/dia_2/apiScript.js
@@ -1,8 +1,14 @@
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
-  // Adicionar lógica aqui!
+const fetchJoke = async () => {
+  const obj = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' },
+  }
+  const response = await fetch(API_URL, obj);
+  const postData = await response.json()
+  console.log(postData.joke)
 };
 
 window.onload = () => fetchJoke();
