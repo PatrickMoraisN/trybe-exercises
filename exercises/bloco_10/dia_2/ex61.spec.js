cfc4e074507a19709e108a328a65ce065b1f4f3c
Patrick Morais <ppternunes@gmail.com>
2021-04-25 17:37:03 -0300

    exercicio 6.1

---
+++ exercises/bloco_10/dia_2/ex61.spec.js
@@ -0,0 +1,36 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (animal) => (
+  new Promise((resolve, reject) => {
+    const findAnimal = Animals.find(elem => elem.name === animal);
+    if (findAnimal) {
+      return resolve(findAnimal);
+    };
+    return reject('Nenhum animal com esse nome!');
+  })
+);
+
+const getAnimal = (animal) => {
+  return findAnimalByName(animal)
+};
+// ---------------------
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', async () => {
+      expect.assertions(1);
+      const animal = await getAnimal('Dorminhoco');
+      expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', async () => {
+        return expect(getAnimal('bob')).rejects.toEqual('Nenhum animal com esse nome!')
+      });
+  });
+});
